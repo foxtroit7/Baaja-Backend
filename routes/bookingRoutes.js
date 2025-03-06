25c358a5de6e92644a5ed39e2d6615c190829340
foxtroit7 <rakeshbihari5301@gmail.com>
2025-03-06 18:32:56 +0530

    verify token added

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const { createBooking, getAllBookings, getUserBookings, getBookingsByArtist, updateBooking, deleteBooking, getBookingById } = require("../controllers/bookingControllers");
+const { verifyToken } = require("../middlewares/verifyToken");
 
 const router = express.Router();
 
-router.post("/create-booking", createBooking);
-router.get("/all-bookings", getAllBookings); 
-router.get("/bookings/:booking_id", getBookingById); 
-router.get("/user-bookings/:user_id", getUserBookings);
-router.get("/artist-bookings/:artist_id", getBookingsByArtist); 
-router.put("/bookings/update/:booking_id",updateBooking); 
-router.delete("/bookings/delete/:booking_id", deleteBooking);
+router.post("/create-booking",verifyToken, createBooking);
+router.get("/all-bookings",verifyToken, getAllBookings); 
+router.get("/bookings/:booking_id",verifyToken, getBookingById); 
+router.get("/user-bookings/:user_id",verifyToken, getUserBookings);
+router.get("/artist-bookings/:artist_id",verifyToken, getBookingsByArtist); 
+router.put("/bookings/update/:booking_id",verifyToken,updateBooking); 
+router.delete("/bookings/delete/:booking_id",verifyToken, deleteBooking);
 module.exports = router;
