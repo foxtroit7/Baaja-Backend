4f1386ea2bcfa49e048d23b717c3774415147501
foxtroit7 <rakeshbihari5301@gmail.com>
2025-03-08 21:16:41 +0530

    22

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use('/api',dashboard);
 app.use('/api', topBaaja);
 app.use('/api', admin);
 app.use('/api', notification);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 22;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
